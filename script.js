385414b9a6eade6d4b794f5671f582c9ddb302c7
sinha08b <161082738+sinha08b@users.noreply.github.com>
2026-04-20 16:48:39 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-window.addEventListener("scroll", () => {
-document.querySelectorAll(".card").forEach(card => {
-let rect = card.getBoundingClientRect();
-if (rect.top < window.innerHeight - 50) {
-card.style.opacity = 1;
-card.style.transform = "translateY(0)";
-}
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute('href'))
+      .scrollIntoView({ behavior: 'smooth' });
+  });
 });
-});
+``
+
